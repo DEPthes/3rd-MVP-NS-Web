c47f51fb03ede69427e27c586fb63d184950ded9
rlotr02 <0314kelee@naver.com>
2024-08-09 12:43:32 +0900

    feat: 날짜 네비게이션 제작

---
+++ src/components/report/DateNavigator.tsx
@@ -0,0 +1,113 @@
+import { SetStateAction, useEffect, useState } from 'react';
+import * as S from '@/styles/report/DateNavigatorStyle';
+import PrevBtn from '@/assets/icons/PrevBtn.svg?react';
+import { TReport } from '@/types/report';
+
+const dummyList = {
+  id: 1,
+  title: '하늘에서 깨끗한 5억원이 떨어진다면?',
+  length: 15,
+  image: '',
+  word: '두기',
+  wordLength: 107,
+  longImage: '',
+  longNickname: '두기두밥두기두밥',
+  longLength: 782,
+  best: [
+    {
+      id: 1,
+      image: '',
+      nickname: '강두기',
+      subject: '비행기를 탔는데 누군가와 자꾸 눈이 마주친다면?',
+      date: '2024. 07. 30',
+      title: '눈 마주친 사람에게 다가가서',
+      heart: 10,
+      selectLength: 5,
+    },
+    {
+      id: 2,
+      image: '',
+      nickname: '정두기',
+      subject: '비행기를 탔는데 누군가와 자꾸 눈이 마주친다면?',
+      date: '2024. 07. 30',
+      title: '윙크를 날려버려~',
+      heart: 13,
+      selectLength: 10,
+    },
+    {
+      id: 3,
+      image: '',
+      nickname: '두기두밥두기두밥',
+      subject: '비행기를 탔는데 누군가와 자꾸 눈이 마주친다면?',
+      date: '2024. 07. 30',
+      title: '운명일지도...',
+      heart: 3,
+      selectLength: 100,
+    },
+  ],
+};
+
+const DateNavigator = ({
+  setReportList,
+}: {
+  setReportList: React.Dispatch<SetStateAction<TReport | null>>;
+}) => {
+  const today = new Date();
+  const [selectedDate, setSelectedDate] = useState<Date>(today);
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  useEffect(() => {
+    //날짜에 맞는 N 보고서 불러오는 API 추가
+    setReportList(dummyList);
+  }, [selectedDate, setReportList]);
+
+  const formatDate = (date: Date) => {
+    return `${date.getFullYear()}년 ${
+      date.getMonth() + 1
+    }월 ${date.getDate()}일`;
+  };
+
+  //이전 날짜 버튼 클릭 함수
+  const handlePrevDay = () => {
+    const prevDay = new Date(selectedDate);
+    prevDay.setDate(selectedDate.getDate() - 1);
+    setIsDisabled(false);
+    setSelectedDate(prevDay);
+  };
+
+  //다음 날짜 버튼 클릭 함수
+  const handleNextDay = () => {
+    if (formatDate(selectedDate) < formatDate(today)) {
+      const nextDay = new Date(selectedDate);
+      nextDay.setDate(selectedDate.getDate() + 1);
+
+      if (formatDate(nextDay) < formatDate(today)) {
+        setIsDisabled(false);
+      } else {
+        setIsDisabled(true);
+      }
+      setSelectedDate(nextDay);
+    }
+  };
+
+  return (
+    <S.Container>
+      <PrevBtn
+        style={{ cursor: 'pointer' }}
+        color="var(--NS-Main1)"
+        onClick={handlePrevDay}
+      />
+      <h1>{formatDate(selectedDate)}</h1>
+      <PrevBtn
+        style={{
+          rotate: '180deg',
+          cursor: isDisabled ? 'default' : 'pointer',
+        }}
+        color={isDisabled ? 'var(--Gray2)' : 'var(--NS-Main1)'}
+        onClick={handleNextDay}
+      />
+    </S.Container>
+  );
+};
+
+export default DateNavigator;
---
+++ src/types/report.ts
@@ -0,0 +1,23 @@
+export type TReport = {
+  id: number;
+  title: string; //주제
+  length: number; //작성된 글 개수
+  image: string; //단어 요약 이미지
+  word: string; //사용자가 가장 많이 쓴 단어
+  wordLength: number; //단어 노출 수
+  longImage: string; //가장 길게 쓴 사람 프로필 사진
+  longNickname: string; //가장 길게 쓴 사람 닉네임
+  longLength: number; //가장 길게 쓴 사람이 쓴 글자수
+  best: TReportBest[];
+};
+
+export type TReportBest = {
+  id: number;
+  image: string; //프로필
+  nickname: string; //닉네임
+  subject: string; //주제
+  date: string; //발행일
+  title: string; //제목
+  heart: number; //좋아요 수
+  selectLength: number; //선정된 횟수
+};
