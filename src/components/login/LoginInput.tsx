15c65b72344ae0d2a82c732379a2b3e280ae0633
rlotr02 <0314kelee@naver.com>
2024-08-11 02:23:10 +0900

    feat: 로그인 반응형 UI 추가

--- src/components/login/LoginInput.tsx
+++ src/components/login/LoginInput.tsx
@@ -1,3 +1,4 @@
+import useNSMediaQuery from '@/hooks/useNSMediaQuery';
 import * as S from '@/styles/login/LoginInputStyle';
 import InputCancelIcon from '@assets/icons/InputCancel.svg?react';
 
@@ -12,6 +13,8 @@ const LoginInput = ({
   setText: (value: string) => void;
   placeholder: string;
 }) => {
+  const { isMobileOrTablet } = useNSMediaQuery();
+
   return (
     <S.InputWrap>
       <S.Input
@@ -24,6 +27,8 @@ const LoginInput = ({
       />
       {text.length > 0 && (
         <InputCancelIcon
+          width={isMobileOrTablet ? 12 : 16}
+          strokeWidth={isMobileOrTablet ? 1 : 2}
           style={{ cursor: 'pointer' }}
           onClick={() => setText('')}
         />
