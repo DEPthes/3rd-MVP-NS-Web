a93a80e7dd27170f5379cd5bda6b638639ada96e
rlotr02 <0314kelee@naver.com>
2024-08-12 10:02:41 +0900

    feat: N보고서 반응형 UI 추가

--- src/styles/report/BestItemStyle.ts
+++ src/styles/report/BestItemStyle.ts
@@ -1,9 +1,11 @@
+import { isMobile, isMobileOrTablet, isTablet } from '@/hooks/Media';
 import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
   justify-content: center;
   gap: 35px;
+  transition: all 0.3s ease-in-out;
 `;
 
 export const ProfileWrap = styled.div`
@@ -12,6 +14,7 @@ export const ProfileWrap = styled.div`
   justify-content: center;
   align-items: center;
   width: 100px;
+  transition: all 0.3s ease-in-out;
 
   > img {
     width: 100px;
@@ -47,11 +50,27 @@ export const PostWrap = styled.div`
   background-color: var(--NS-White);
   box-shadow: 0px 0px 20px rgba(94, 114, 235, 0.5);
   border-radius: 40px;
-  padding: 31px 50px 23px 46px;
+  padding: 0 46px;
+  transition: all 0.3s ease-in-out;
+
+  ${isMobile} {
+    width: 320px;
+  }
+
+  ${isTablet} {
+    width: 728px;
+  }
+
+  ${isMobileOrTablet} {
+    height: 76px;
+    border-radius: 20px;
+    padding: 0 20px;
+  }
 `;
 
 export const PostLeftWrap = styled.div`
   width: 93%;
+  transition: all 0.3s ease-in-out;
 
   > h3 {
     font: var(--H3);
@@ -59,11 +78,19 @@ export const PostLeftWrap = styled.div`
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
+
+    ${isMobileOrTablet} {
+      font: var(--P-S1);
+    }
   }
 
   > p {
     font: var(--S1-1);
     color: var(--Gray1);
+
+    ${isMobileOrTablet} {
+      font: var(--P-S2-2);
+    }
   }
 `;
 
@@ -71,9 +98,21 @@ export const PostRightWrap = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  transition: all 0.3s ease-in-out;
+
+  svg {
+    ${isMobileOrTablet} {
+      margin-top: -11px;
+    }
+  }
 
   > h5 {
     font: var(--S1);
     color: var(--NS-Main1);
+
+    ${isMobileOrTablet} {
+      font: var(--T5-B);
+      margin-top: -11px;
+    }
   }
 `;
